baafa185e549a18fad786bfb36c11b7188d0812e
Clivern <hello@clivern.com>
2017-07-10 17:25:28 +0200

    new fixes

--- src/main/java/com/clivern/racter/senders/templates/Message.java
+++ src/main/java/com/clivern/racter/senders/templates/Message.java
@@ -114,31 +114,29 @@ public class Message {
 
     public String build() throws IOException
     {
+		/*
+		 	{
+		 	  	"recipient": {
+		 	    	"id": "USER_ID"
+		 	  	},
+		 	  	"message": {
+		 	    	"text": "hello, world!",
+		 			"attachment":{
+		 	      		"type":"audio", # file, audio, image, video
+		 	      		"payload":{
+		 	        		"url":"https://petersapparel.com/bin/clip.mp3",
+		 	        		"is_reusable": true # Optional You can optimize sending multimedia by reusing attachments. If you're sending the same attachments repeatedly, you should consider reusing them. Attachment reuse works with sending images, audio clips, videos and files.
+		 	      		}
+		 	    	},
+		 	    	"quick_replies":[
 
-/*
- 	{
- 	  	"recipient": {
- 	    	"id": "USER_ID"
- 	  	},
- 	  	"message": {
- 	    	"text": "hello, world!",
- 			"attachment":{
- 	      		"type":"audio", # file, audio, image, video
- 	      		"payload":{
- 	        		"url":"https://petersapparel.com/bin/clip.mp3",
- 	        		"is_reusable": true # Optional You can optimize sending multimedia by reusing attachments. If you're sending the same attachments repeatedly, you should consider reusing them. Attachment reuse works with sending images, audio clips, videos and files.
- 	      		}
- 	    	},
- 	    	"quick_replies":[
-
- 	    	],
- 	    	"metadata": "" # Custom string that is delivered as a message echo.
- 	  	}
-
- 	  	"sender_action":"typing_on", # typing_on, typing_off, mark_seen
- 	  	"notification_type": "REGULAR" # REGULAR, SILENT_PUSH, NO_PUSH
- 	}
- */
+		 	    	],
+		 	    	"metadata": "" # Custom string that is delivered as a message echo.
+		 	  	}
+		 	  	"sender_action":"typing_on", # typing_on, typing_off, mark_seen
+		 	  	"notification_type": "REGULAR" # REGULAR, SILENT_PUSH, NO_PUSH
+		 	}
+		*/
  		this.message_string  = "{";
 
  		if( this.recipient_id != null ){
