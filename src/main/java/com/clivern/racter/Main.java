d0d4a6f3bc1db22092e3d4b2c499f23b48d745eb
Clivern <hello@clivern.com>
2017-07-10 19:46:27 +0200

    base sender added to main

--- src/main/java/com/clivern/racter/Main.java
+++ src/main/java/com/clivern/racter/Main.java
@@ -14,37 +14,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.io.IOException;
 
-/*
-import com.fasterxml.jackson.jr.ob.*;
-*/
-/*import java.io.IOException;
-
-import com.fasterxml.jackson.jr.ob.*;
-
-import com.mashape.unirest.http.*;
-import com.mashape.unirest.http.async.Callback;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.http.options.Options;
-import com.mashape.unirest.request.GetRequest;
-import com.mashape.unirest.request.HttpRequest;*/
-
-
 public class Main {
 
     public static void main(String[] args) throws IOException
     {
         get("/", (request, response) -> {
-
-            MessageTemplate message = BaseSender.getInstance().getMessageTemplate();
-            message.setRecipientId("tw7w8egdhsjsd");
-            message.setMessageText("Hello World!");
-            message.setMessageMetadata("Metadata");
-            message.setSenderAction("Sender Action");
-            message.setNotificationType("Notif Type");
-
-            return message.build();
-
-/*            BotPlatform platform = BotPlatform.getInstance().configDependencies().loadConfigs("src/main/java/resources/config.properties");
+            BotPlatform platform = BotPlatform.getInstance().configDependencies().loadConfigs("src/main/java/resources/config.properties");
             platform.getVerifyWebhook().setHubMode(( request.queryParams("hub.mode") != null ) ? request.queryParams("hub.mode") : "");
             platform.getVerifyWebhook().setHubVerifyToken(( request.queryParams("hub.verify_token") != null ) ? request.queryParams("hub.verify_token") : "");
             platform.getVerifyWebhook().setHubChallenge(( request.queryParams("hub.challenge") != null ) ? request.queryParams("hub.challenge") : "");
@@ -57,7 +32,7 @@ public class Main {
 
             platform.finish();
             response.status(403);
-            return "Verification token mismatch";*/
+            return "Verification token mismatch";
 
         });
 
@@ -71,7 +46,11 @@ public class Main {
             platform.getBaseReceiver().set(body).parse();
             HashMap<String, MessageReceivedWebhook> messages = (HashMap<String, MessageReceivedWebhook>) platform.getBaseReceiver().getMessages();
             for (MessageReceivedWebhook message : messages.values()) {
-                return message.getMessageText();
+                MessageTemplate message_tpl = BaseSender.getInstance().getMessageTemplate();
+                message_tpl.setRecipientId(message.getUserId());
+                message_tpl.setMessageText(message.getMessageText());
+                BaseSender.getInstance().sendMessageTemplate(message_tpl);
+                return "ok";
             }
             return "bla";
         });
--- src/main/java/com/clivern/racter/senders/BaseSender.java
+++ src/main/java/com/clivern/racter/senders/BaseSender.java
@@ -5,6 +5,13 @@ package com.clivern.racter.senders;
 
 import com.clivern.racter.senders.templates.*;
 
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.http.options.Options;
+import com.mashape.unirest.request.GetRequest;
+import com.mashape.unirest.request.HttpRequest;
+
 /**
  * Base Sender Class
  */
@@ -13,7 +20,6 @@ public class BaseSender {
 	private static BaseSender instance;
 	private String remote_url = "https://graph.facebook.com/v2.6/me/messages?access_token=";
 	private String access_token;
-	private MessageTemplate message_template;
 
 	/**
 	 * Constructor
@@ -32,9 +38,20 @@ public class BaseSender {
 	    return instance;
 	}
 
-	public MessageTemplate getMessageTemplate()
-	{
-		this.message_template = new MessageTemplate();
-		return this.message_template;
+	public void setAccessToken(String access_token){
+		this.access_token = access_token;
+	}
+
+	public MessageTemplate getMessageTemplate(){
+		return new MessageTemplate();
+	}
+
+	public Boolean sendMessageTemplate(MessageTemplate message_template) throws UnirestException {
+		String url = this.remote_url + this.access_token;
+		String body = message_template.build();
+
+		HttpResponse<String> response = Unirest.post(url).header("Content-Type", "application/json").body(body).asString();
+
+		return true;
 	}
 }
