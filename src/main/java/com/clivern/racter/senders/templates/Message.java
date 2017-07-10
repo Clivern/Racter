f5c2e4df861b0febeb625cede3fef0aa7698c565
Clivern <hello@clivern.com>
2017-07-10 16:53:34 +0200

    new fix

--- src/main/java/com/clivern/racter/senders/templates/Message.java
+++ src/main/java/com/clivern/racter/senders/templates/Message.java
@@ -197,34 +197,6 @@ public class Message {
 
  		this.message_string += "}";
 
-/*        String this.message_string = JSON.std.with(JSON.Feature.PRETTY_PRINT_OUTPUT).composeString()
-        	.startObject()
-                .startObjectField("recipient")
-                    .put("id", "USER_ID")
-                .end()
-                .startObjectField("message")
-                    .startObjectField("attachment")
-                        .put("type", "template")
-                        .startObjectField("payload")
-                            .put("template_type", "button")
-                            .put("text", "What do you want to do next?")
-                            .startArrayField("buttons")
-                          .startObject()
-                              .put("type", "web_url")
-                              .put("url", "https://petersapparel.parseapp.com")
-                              .put("title", "Show Website")
-                          .end()
-                          .startObject()
-                              .put("type", "postback")
-                              .put("title", "Start Chatting")
-                              .put("payload", "USER_DEFINED_PAYLOAD")
-                          .end()
-                            .end()
-                        .end()
-                    .end()
-                .end()
-            .end()
-            .finish();*/
         return this.message_string;
     }
 
