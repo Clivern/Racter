16a1224e672f3982143dda9b438e17168d11df46
Clivern <hello@clivern.com>
2017-10-10 15:44:53 +0200

    Post Back Webhook added

--- src/main/java/com/clivern/racter/receivers/webhook/PostbackWebhook.java
+++ src/main/java/com/clivern/racter/receivers/webhook/PostbackWebhook.java
@@ -32,4 +32,150 @@ public class PostbackWebhook {
         }
     }
     */
+
+    /**
+     * @var String
+     */
+    protected String user_id;
+
+    /**
+     * @var String
+     */
+    protected String page_id;
+
+    /**
+     * @var Long
+     */
+    protected Long timestamp;
+
+    /**
+     * @var Map<String, String>
+     */
+    protected Map<String, String> postback = new HashMap<String, String>();
+
+
+    /**
+     * Set User ID
+     *
+     * @param user_id
+     * @return void
+     */
+    public void setUserId(String user_id)
+    {
+        this.user_id = user_id;
+    }
+
+    /**
+     * Set Page ID or Receiver ID
+     *
+     * @param page_id
+     * @return void
+     */
+    public void setPageId(String page_id)
+    {
+        this.page_id = page_id;
+    }
+
+    /**
+     * Set Timestamp
+     *
+     * @param timestamp
+     * @return void
+     */
+    public void setTimestamp(Long timestamp)
+    {
+        this.timestamp = timestamp;
+    }
+
+    /**
+     * Set Postback
+     *
+     * @param key
+     * @param value
+     * @return void
+     */
+    public void setPostback(String key, String value)
+    {
+        postback.put(key, value);
+    }
+
+    /**
+     * Check if User ID Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasUserId()
+    {
+        return (this.user_id != null);
+    }
+
+    /**
+     * Check if Page ID Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasPageId()
+    {
+        return (this.page_id != null);
+    }
+
+    /**
+     * Check if Timestamp Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasTimestamp()
+    {
+        return (this.timestamp != null);
+    }
+
+    /**
+     * Check if Postback Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasPostback()
+    {
+        return !this.postback.isEmpty();
+    }
+
+    /**
+     * Get User ID
+     *
+     * @return String
+     */
+    public String getUserId()
+    {
+        return this.user_id;
+    }
+
+    /**
+     * Get Page ID
+     *
+     * @return String
+     */
+    public String getPageId()
+    {
+        return this.page_id;
+    }
+
+    /**
+     * Get Timestamp
+     *
+     * @return Long
+     */
+    public Long getTimestamp()
+    {
+        return this.timestamp;
+    }
+
+    /**
+     * Get Postback
+     *
+     * @return Map<String, String>
+     */
+    public Map<String, String> getPostback()
+    {
+        return this.postback;
+    }
 }
