843313179f97d16a47ddd14dbdbaab213ba7d385
Clivern <hello@clivern.com>
2017-10-11 15:15:41 +0200

    receiver interface added

--- src/main/java/com/clivern/racter/contract/templates/ReceiverTemplate.java
+++ src/main/java/com/clivern/racter/contract/templates/ReceiverTemplate.java
@@ -9,4 +9,45 @@ package com.clivern.racter.contract.templates;
  */
 public interface ReceiverTemplate {
 
+    /**
+     * Set User ID
+     *
+     * @param user_id
+     */
+    public void setUserId(String user_id);
+
+    /**
+     * Set Page ID or Receiver ID
+     *
+     * @param page_id
+     */
+    public void setPageId(String page_id);
+
+    /**
+     * Check if User ID Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasUserId();
+
+    /**
+     * Check if Page ID Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasPageId();
+
+    /**
+     * Get User ID
+     *
+     * @return String
+     */
+    public String getUserId();
+
+    /**
+     * Get Page ID
+     *
+     * @return String
+     */
+    public String getPageId();
 }
--- src/main/java/com/clivern/racter/receivers/webhook/MessageEchoWebhook.java
+++ src/main/java/com/clivern/racter/receivers/webhook/MessageEchoWebhook.java
@@ -14,4 +14,68 @@ import org.json.JSONArray;
  */
 public class MessageEchoWebhook implements ReceiverTemplate {
 
+
+    protected String user_id;
+
+    protected String page_id;
+
+    /**
+     * Set User ID
+     *
+     * @param user_id
+     */
+    public void setUserId(String user_id)
+    {
+        this.user_id = user_id;
+    }
+
+    /**
+     * Set Page ID or Receiver ID
+     *
+     * @param page_id
+     */
+    public void setPageId(String page_id)
+    {
+        this.page_id = page_id;
+    }
+
+    /**
+     * Check if User ID Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasUserId()
+    {
+        return (this.user_id != null);
+    }
+
+    /**
+     * Check if Page ID Exist
+     *
+     * @return Boolean
+     */
+    public Boolean hasPageId()
+    {
+        return (this.page_id != null);
+    }
+
+    /**
+     * Get User ID
+     *
+     * @return String
+     */
+    public String getUserId()
+    {
+        return this.user_id;
+    }
+
+    /**
+     * Get Page ID
+     *
+     * @return String
+     */
+    public String getPageId()
+    {
+        return this.page_id;
+    }
 }
