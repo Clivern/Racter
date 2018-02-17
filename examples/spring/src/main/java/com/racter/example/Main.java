282c09dafb5a1a010e070ac072315e3cb85ace90
Clivern <hello@clivern.com>
2018-02-17 11:36:15 +0100

    Update copyright info

--- examples/spring/src/main/java/com/racter/example/Main.java
+++ examples/spring/src/main/java/com/racter/example/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 A.F <http://clivern.com>
+ * Copyright (C) 2017 Clivern <http://clivern.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
